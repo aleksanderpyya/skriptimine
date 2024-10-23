b80a4d8317833eeee539b515f8f68bb71e7bbda1
Aleksander Püüa <aleksander.puua@voco.ee>
2024-10-23 13:50:25 +0300

    lisatud remote seadistus

---
+++ praks1/git_kasutamine.sh
@@ -0,0 +1,10 @@
+!#/bin/bash
+### See on esimene skript.
+echo -n "Oled kasutaja nimega: "
+whoami
+sleep 1
+echo -n "ja sina asud kataloogis:"
+pwd
+sleep 1
+echo "selle kataloogi sisu(list) on: "
+ls -l
